f108f530c2b4022133be140af7fa1b5f02690040
SampaioAllan <lanbrendo93@gmail.com>
2022-05-02 22:05:04 -0300

    adicionando o construtor e parte do método que não foi incluso no ultio commit

--- Pessoa.cs
+++ Pessoa.cs
@@ -5,6 +5,13 @@ namespace Concessionaria
         public string Nome { get; set; }
         public string CPF { get; set; }
         public DateTime DataNasc { get; set; }
+        public Pessoa (string nome, string cpf, string dataNasc)
+        {
+            SetNome(nome);
+            SetCPF(cpf);
+            SetDataNasc(dataNasc);
+    
+        }
 
         public void SetNome(string nome)
         {
@@ -24,7 +31,12 @@ namespace Concessionaria
         }
         public void SetDataNasc(string dataNasc)
         {
-            
+            var dataNascDateTime=DateTime.Parse(dataNasc);
+            DataNasc=dataNascDateTime;
+        }
+        public DateTime GetDataNasc()
+        {
+            return DataNasc;
         }
     }
 }
