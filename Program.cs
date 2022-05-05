e63e1b095381338b496d760b8753aabfb762a6c8
SampaioAllan <lanbrendo93@gmail.com>
2022-05-05 00:33:00 -0300

    criando um pequeno teste na program para garantir que nada esta quebrando

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Concessionaria
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var carroTest = new Carro("gm", "onix", "01/01/2020", 0, "preto", 10000, true, "flex");
+            var motoTest = new Moto("honda", "twister", "01/01/2020",0,"preta", 1, 250, "injeção eletrônica");
+            Console.WriteLine($"O carro tem cambio automatico? {carroTest.GetTransmissaoAutomatica()}. Valor do carro é:{carroTest.GetValor()}");
+            Console.WriteLine($"A moto tem partida: {motoTest.GetPartida()}. Valor da moto é:{motoTest.GetValor()}");
+        }
+    }
+}
